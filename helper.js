f767923553b0745efc7e20d7c5924f457a486bd7
nael05 <nael.morellon@ynov.com>
2026-03-25 15:35:02 +0100

    get

---
+++ helper.js
@@ -0,0 +1,22 @@
+/*
+const success =(message, data) =>{
+    return {
+        message: message,
+        data: data
+    }
+}
+
+module.exports = success;
+*/
+/*
+exports.success = (message, data) => {
+    return {
+        message: message,
+        data: data
+    }
+}
+*/
+exports.success = (message, data) => {
+    return {message, data};
+};
+
